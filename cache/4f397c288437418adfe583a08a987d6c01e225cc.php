a8ce2fbd1e7df691403e266e285a67a766b31e93
zhorton <zak.horton@vml.com>
2020-03-29 03:15:06 -0500

    Added autoloading

---
+++ cache/4f397c288437418adfe583a08a987d6c01e225cc.php
@@ -0,0 +1,4 @@
+<p>
+    <?php echo e($person); ?> Home Page
+</p>
+<?php /**PATH /Users/zhorton/tutorials/authorize-slim-4/resources/views/sections/person.blade.php ENDPATH**/ ?>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <title>
+            Slim 4 Authentication
+        </title>
+    </head>
+    <body>
+        <div>
+            <h1>Hello World Of Blade Templates!</h1>
+            @yield('content')
+        </div>
+    </body>
+</html>
