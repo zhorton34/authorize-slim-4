e8dbe3d27fa8648e419df0b74d5439e9247c7b15
zhorton <zak.horton@vml.com>
2020-04-09 02:53:25 -0500

    Refactored console entry point

--- routes/console.php
+++ routes/console.php
@@ -2,8 +2,9 @@
 
 use App\Support\Console\Console;
 
-Console::command('make:test {name} {config}',
-    fn () => $this->info($this->input->getArgument('name'))
-)
-->setDescription('Make Test Scaffold File');
+Console::command('make:test {name} {config}', function () {
+    $name = $this->input->getArgument('name');
+
+    $this->info($name);
+})->setDescription('Make Test Scaffold File');
 
