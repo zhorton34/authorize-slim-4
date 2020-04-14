01fe45d335f407facb78f185cdcce7a525206452
zhorton <zak.horton@vml.com>
2020-04-13 22:05:47 -0500

    added tailwind initial scss

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="bg-white shadow-md hover:shadow-xl rounded-lg border-2 w-1/2 p-4 mt-10">
+        <h1 class="text-6xl text-gray-800 font-semibold">
+            Slim 4 Authentication
+        </h1>
+        <h2 class="text-2xl">
+            Welcome to our Slim 4 Authentication Tutorial
+        </h2>
+    </div>
+@endsection
